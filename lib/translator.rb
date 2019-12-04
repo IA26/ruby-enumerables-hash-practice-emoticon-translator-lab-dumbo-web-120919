b88e49338570d9cdde730215de9ab8dc438e9c02
Isaac Avilez <isaac1180@aol.com>
2019-12-04 19:59:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,4 +26,3 @@ def get_english_meaning(file_path, emoticon)
   result = load_library(file_path)['get_meaning'][emoticon]
   result ? result : "Sorry, that emoticon was not found"
 end
-© 2019 GitHub, Inc.
